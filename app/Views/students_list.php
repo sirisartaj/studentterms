1dc68f85ccbeaeaae0e9c500d69822bfc9e4e7ec
sirisartaj <116791477+sirisartaj@users.noreply.github.com>
2022-11-10 14:33:36 +0530

    student term due text added
    student term due text added

--- app/Views/students_list.php
+++ app/Views/students_list.php
@@ -13,6 +13,7 @@
 <link href="https://sirians.xyz/dgtl/css/ui.css" rel="stylesheet">
 <link href="https://sirians.xyz/dgtl/css/layout.css" rel="stylesheet">
 <link href="https://sirians.xyz/dgtl/css/custom.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://sirians.xyz/dgtl/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 </head>
 <!-- BEGIN BODY -->
@@ -164,7 +165,7 @@
     <!-- BEGIN PAGE CONTENT -->
     <div class="page-content">
       <div class="header">
-        <h2><strong>Enquiries</strong></h2>
+        <h2><strong>Student Trem Dues</strong></h2>
         <div class="breadcrumb-wrapper">
           <ol class="breadcrumb">
             <li><a href="https://sirians.xyz/dgtl/dashboard.html">Home</a> </li>
@@ -181,7 +182,7 @@
       <div class="col-sm-3">
                 <div class="form-group">
         <label class="control-label">Terms</label>
-           <select id="SelectType" name="SelectType" class="form-control form-white" required>
+           <select id="SelectType" name="SelectType" class="form-control form-white" required onChange="searchtermchnage();">
           <?php foreach($terms as $t){ ?> 
            <option value="<?php echo $t['term_name']; ?>"><?php echo $t['term_name']; ?></option>
          <?php } ?>
@@ -353,53 +354,11 @@
   <!-- END MAIN CONTENT --> 
 </section>
 <a href="https://sirians.xyz/dgtl/#" class="scrollup"><i class="fa fa-angle-up"></i></a> 
-<script src="https://sirians.xyz/dgtl/js/jquery-1.11.1.min.js"></script> 
-<script src="https://sirians.xyz/dgtl/js/jquery-migrate-1.2.1.min.js"></script> 
-<script src="https://sirians.xyz/dgtl/js/jquery-ui-1.11.2.min.js"></script> 
-<script src="https://sirians.xyz/dgtl/js/main-gsap.min.js"></script> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/bootstrap.min.js"></script> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/jquery.cookies.min.js"></script> <!-- Jquery Cookies, for theme --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/jquery.blockUI.min.js"></script> <!-- simulate synchronous behavior when using AJAX --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/bootbox.min.js"></script> <!-- Modal with Validation --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/jquery.mCustomScrollbar.concat.min.js"></script> <!-- Custom Scrollbar sidebar --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/bootstrap-hover-dropdown.min.js"></script> <!-- Show Dropdown on Mouseover --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/sparkline.min.js"></script> <!-- Charts Sparkline --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/select2.min.js"></script> <!-- Select Inputs --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/icheck.min.js"></script> <!-- Checkbox & Radio Inputs --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/backstretch.min.js"></script> <!-- Background Image --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/bootstrap-progressbar.min.js"></script> <!-- Animated Progress Bar --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/chart.min.js"></script> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/sidebar_hover.js"></script> <!-- Sidebar on Hover --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/notes.js"></script> <!-- Notes Widget --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/quickview.js"></script> <!-- Chat Script --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/search.js"></script> <!-- Search Script --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/plugins.js"></script> <!-- Main Plugin Initialization Script --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/application.js"></script> <!-- Main Application Script --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/nprogress.js"></script> <!-- Progress bar on page load --> 
-
-<!-- BEGIN PAGE SCRIPT -->
-<link rel="stylesheet" type="text/css" href="https://sirians.xyz/dgtl/js/tables/buttons.dataTables.min.css">
-<!-- data tables css --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/tables/jquery.dataTables.min.js"></script> <!-- Datatables Main File --> 
-<!-- <script type="text/javascript" src="https://sirians.xyz/dgtl/js/tables/dataTables.responsive.min.js"></script>--> <!-- Datatables Responsive --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/tables/dataTables.buttons.min.js"></script> <!-- Datatables Buttons --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/tables/jszip.min.js"></script> <!-- Datatables copy Button --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/tables/pdfmake.min.js"></script> <!-- Datatables PDF Button --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/tables/vfs_fonts.js"></script> <!-- Datatables CSV Button --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/tables/buttons.html5.min.js"></script><!-- Datatables Buttons HTML5 --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/tables/buttons.print.min.js"></script> <!-- Datatables Print Button --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/bootstrap-datepicker.js"></script> <!-- Bootstrap Date Picker --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/bootstrap-datetimepicker-new.js"></script> <!-- Bootstrap New Date Picker --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/bootstrap-tagsinput.min.js"></script> <!-- Select Inputs --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/jquery.validate.min.js"></script><!-- Validation --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/dropzone.min.js"></script> <!-- Upload Image & File in dropzone --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/typed.min.js"></script> <!-- Animated Typing --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/summernote.min.js"></script> <!-- Simple HTML Editor --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/editor.js"></script> 
-<!-- BEGIN END SCRIPT --> 
-
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/layout.js"></script> <!-- Main Application Script --> 
-<script type="text/javascript" src="https://sirians.xyz/dgtl/js/custom.js"></script> <!-- custom file -->
-
+<?php echo view('js_footer');?>
+<script>
+  function searchtermchnage(){
+    alert('hi');
+  }
+</script>
 </body>
 </html>
